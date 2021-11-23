860a5b9d2fd48b731e84d3c73d2693a97a530de0
Liesbeth Bogaert <liesbeth@dataroots.io>
2021-11-23 14:42:12 +0100

    Function deployment without publish profile

--- function/BlobTrigger/__init__.py
+++ function/BlobTrigger/__init__.py
@@ -5,6 +5,7 @@ import requests
 import azure.functions as func
 
 
+"Test function deployment"
 def main(myblob: func.InputStream):
     """Sends a request to the Machine learning model once activated by blob storage event
     Args:
